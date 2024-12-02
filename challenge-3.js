4283a45a919af0ed54ca6cbfc61363dd79f5b1bc
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-12-02 17:58:20 +0300

    Challenge 3 updated ---Prompt included & Benefits added---

--- challenge-3.js
+++ challenge-3.js
@@ -1,81 +1,94 @@
-// The Variable basicSalary is declared, this variable holds the value of an individual's salary before deductions;
-let basicSalary;
+// Setting up the prompt module to prompt the user to input basic salary and benefits, you have to install the "prompt-sync" module (see README file)
+const prompt = require("prompt-sync")();
+let salaryInput = Number(prompt("Please enter your basic salary: ", ""))
+let benefitsInput = Number(prompt("Please enter your total benefits: ", ""))
+
+// Function userPrompt returns the value that has been put in by the user
+function userPrompt() {
+    return salaryInput, benefitsInput;
+}
+
+// User prompt is called first to trigger the input prompt 
+userPrompt();
+
+// The Variable basicSalary is declared, this variable holds the value of an individual's basic salary before deductions
+const basicSalary = salaryInput;
+
+// The Variable totalBenefits is declared, this variable holds the value of an individual's total benefits before deductions
+const totalBenefits = benefitsInput;
+
+// Variable grossSalary holds the value of the Gross Salary which is equal to the total of the basic salary and the total benefits
+const grossSalary = basicSalary + totalBenefits;
 
 // Function payeDeductions calculates the expected Pay-As-You-Earn Tax deductions
-const payeDeductions = function (basicSalary) {
+const payeDeductions = function (grossSalary) {
     // These conditions check the salary scale and perform the respective tax rate
-    if (basicSalary > 800000) {
-        return basicSalary * 0.35;
-    } else if (basicSalary >= 500001 && basicSalary <= 800000) {
-        return basicSalary * 0.325;
-    } else if (basicSalary >= 32334 && basicSalary <= 500000) {
-        return basicSalary * 0.3;
-    } else if (basicSalary >= 24001 && basicSalary <= 32333) {
-        return basicSalary * 0.25;
-    } else if (basicSalary <= 24000) {
-        return basicSalary * 0.1;
+    if (grossSalary > 800000) {
+        return grossSalary * 0.35;
+    } else if (grossSalary >= 500001 && grossSalary <= 800000) {
+        return grossSalary * 0.325;
+    } else if (grossSalary >= 32334 && grossSalary <= 500000) {
+        return grossSalary * 0.3;
+    } else if (grossSalary >= 24001 && grossSalary <= 32333) {
+        return grossSalary * 0.25;
+    } else if (grossSalary <= 24000) {
+        return grossSalary * 0.1;
     } else return 'Put in the salary as a number!'
 
 }
 
 
 // Function nhifDeductions matches the salary to the corresponding deduction
-const nhifDeductions = function (basicSalary) {
+const nhifDeductions = function (grossSalary) {
     // These conditions check the salary scale and match it to its respective deduction
-    if (basicSalary > 100000) {
+    if (grossSalary > 100000) {
         return 1700
-    } else if (basicSalary >= 90000 && basicSalary <= 99999) {
+    } else if (grossSalary >= 90000 && grossSalary <= 99999) {
         return 1600;
-    } else if (basicSalary >= 80000 && basicSalary <= 89999) {
+    } else if (grossSalary >= 80000 && grossSalary <= 89999) {
         return 1500;
-    } else if (basicSalary >= 70000 && basicSalary <= 79999) {
+    } else if (grossSalary >= 70000 && grossSalary <= 79999) {
         return 1400;
-    } else if (basicSalary >= 60000 && basicSalary <= 69999) {
+    } else if (grossSalary >= 60000 && grossSalary <= 69999) {
         return 1300;
-    } else if (basicSalary >= 50000 && basicSalary <= 59999) {
+    } else if (grossSalary >= 50000 && grossSalary <= 59999) {
         return 1200;
-    } else if (basicSalary >= 45000 && basicSalary <= 49999) {
+    } else if (grossSalary >= 45000 && grossSalary <= 49999) {
         return 1100;
-    } else if (basicSalary >= 40000 && basicSalary <= 44999) {
+    } else if (grossSalary >= 40000 && grossSalary <= 44999) {
         return 1000;
-    } else if (basicSalary >= 35000 && basicSalary <= 39999) {
+    } else if (grossSalary >= 35000 && grossSalary <= 39999) {
         return 950;
-    } else if (basicSalary >= 30000 && basicSalary <= 34999) {
+    } else if (grossSalary >= 30000 && grossSalary <= 34999) {
         return 900;
-    } else if (basicSalary >= 25000 && basicSalary <= 29999) {
+    } else if (grossSalary >= 25000 && grossSalary <= 29999) {
         return 850;
-    } else if (basicSalary >= 20000 && basicSalary <= 24999) {
+    } else if (grossSalary >= 20000 && grossSalary <= 24999) {
         return 750;
-    } else if (basicSalary >= 15000 && basicSalary <= 19999) {
+    } else if (grossSalary >= 15000 && grossSalary <= 19999) {
         return 600;
-    } else if (basicSalary >= 12000 && basicSalary <= 14999) {
+    } else if (grossSalary >= 12000 && grossSalary <= 14999) {
         return 500;
-    } else if (basicSalary >= 8000 && basicSalary <= 11999) {
+    } else if (grossSalary >= 8000 && grossSalary <= 11999) {
         return 400;
-    } else if (basicSalary >= 6000 && basicSalary <= 7999) {
+    } else if (grossSalary >= 6000 && grossSalary <= 7999) {
         return 300;
-    } else if (basicSalary <= 5999) {
+    } else if (grossSalary <= 5999) {
         return 150;
     } else return 'Put in the salary as a number!'
 }
 
 
 // Function nssfDeductions calculates the expected nssf deduction from the salary
-const nssfDeductions = function (basicSalary) {
-    if (basicSalary <= 7000) {
-        return basicSalary * 0.06;
-    } else if (basicSalary >= 7001 && basicSalary <= 36000) {
-        return basicSalary * 0.06;
-    } else if (basicSalary > 36000) {
+const nssfDeductions = function (grossSalary) {
+    if (grossSalary <= 7000) {
+        return grossSalary * 0.06;
+    } else if (grossSalary >= 7001 && grossSalary <= 36000) {
+        return grossSalary * 0.06;
+    } else if (grossSalary > 36000) {
         return 0;
-    } else return 'Put in the salary as a number!'
+    }
 }
-
-// Testing the functions with a salary of 1,000,000 (You can change to any value)
-basicSalary = 1000000;
-
-// Variable grossSalary holds the value of the Gross Salary which is equal to variable basicSalary
-const grossSalary = basicSalary;
 
 // Variable totalDeductions holds the value of the all the deductions summed up
 const totalDeductions = payeDeductions(grossSalary) + nhifDeductions(grossSalary) + nssfDeductions(grossSalary);
@@ -83,5 +96,10 @@ const totalDeductions = payeDeductions(grossSalary) + nhifDeductions(grossSalary
 // Variable netSalary holds the value of the Net Salary which is the difference of the Gross Salary and the deductions
 const netSalary = grossSalary - totalDeductions;
 
-console.log(`Gross Salary: ${grossSalary}`)
-console.log(`Net Salary: ${netSalary}`)
+// This conditions checks whether the prompt input exists and is a number then prints out the Gross Salary and the Net Salary after deductions
+if (grossSalary) {
+    console.log(`Gross Salary: ${grossSalary}`);
+    console.log(`Net Salary: ${netSalary}`);
+} else if (!grossSalary) {
+    console.log('Put in the salary as a number!')
+}
